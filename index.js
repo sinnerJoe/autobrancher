b64244f21d08f08742e7b88f27eb4499348d74b1
Radu Cebotari <radu.cebotari@admin-me.com>
2019-08-30 11:41:04 +0300

    added 'clean' argument command

--- index.js
+++ index.js
@@ -68,27 +68,49 @@ const branchExistsRemotely = (soughtBranch) => {
     return false
 }
 
-
-let mainBranch;
-if(process.argv.length === 3 && process.argv[2].match(/(\w|[-_])+/)){
-    mainBranch = process.argv[2];
-} else if (process.argv.length > 3){
-    throw "Too many arguments"
-} else {
+const findMainBranch = () => {
     const toDevReg = /([\d\w-_]+?)_to_dev/;
     const toQaReg = /([\d\w-_]+?)_to_qa/;
-    mainBranch = getCurrentBranch()
-    for(const reg of [toDevReg, toQaReg]){
+    let mainBranch = getCurrentBranch()
+    for (const reg of [toDevReg, toQaReg]) {
         if (mainBranch.match(reg)) {
-            mainBranch = mainBranch.replace(reg, "$1");
-            moveToBranch(mainBranch);
+            let mainBranch = mainBranch.replace(reg, "$1");
             break;
         }
     }
+    return mainBranch
 }
+
+
+if (process.argv.length > 3) {
+    throw "Too many arguments"
+} 
+
+
+let mainBranch = findMainBranch();
 
 const toDevBranch = `${mainBranch}_to_dev`;
 const toQaBranch = `${mainBranch}_to_qa`;
+
+if (process.argv.length === 3) {
+    switch (process.argv[2]) {
+        case 'clean': 
+            moveToBranch(mainBranch);
+            for(const branch of [toDevBranch, toQaBranch]){
+                try{
+                    deleteBranch(branch);
+                }catch(err){}
+            }
+            process.exit(0);
+    }
+} 
+
+if(['master', 'qa', 'development'].includes(mainBranch)){
+    console.log(`You can't create secondary branches from ${mainBranch}.`)
+    console.log(`Please move to the branch of your task`)
+    process.exit(1);
+    return 1;
+}
 
 fetchRemoteBranches();
 
@@ -101,41 +123,40 @@ try{
 
 
 
-function publishTemportaryBranch(branch, pullTarget){
+function publishTemportaryBranch(branch, pullTarget) {
 
     let stage = 0
 
-    try{
-        
+    try {
+
         console.log(`mainBranch = ${mainBranch}`)
         moveToBranch(mainBranch)
-        
-        if(!branchExistsLocaly(branch)){
+
+        if (!branchExistsLocaly(branch)) {
             console.log(`Branch ${branch} created!`)
             createBranch(branch)
         }
-        else {   
+        else {
             moveToBranch(branch)
         }
-        
+
         stage = 1;
-        if(branchExistsRemotely(branch)) pullFrom(branch);
+        if (branchExistsRemotely(branch)) pullFrom(branch);
         stage = 2
         pullFrom(pullTarget)
         stage = 3
         mergeWith(mainBranch)
-        const result = pushTo(branch)
+        pushTo(branch)
         moveToBranch(mainBranch)
         deleteBranch(branch)
-        // return result.match(/^remote:\s+(https:\/\/.+)$/i);
-    }catch(err){
+    } catch (err) {
         console.log("ERROR " + err.message);
-        if (err.message.includes("Command failed: git pull origin")){
+        if (err.message.includes("Command failed: git pull origin")) {
             console.log(`Conflict on branch "${branch}" after pulling from "${pullTarget}"`)
             throw err
         }
 
-        switch(stage){
+        switch (stage) {
             case 1: console.log(`Conflict on pulling remote branch origin/${branch}`); throw err;
             case 2: console.log(`Conflict on pulling remote branch origin/${pullTarget} into ${branch}`); throw err;
             case 3: console.log(`Conflict on merge of ${mainBranch} into ${branch}`); throw err;
